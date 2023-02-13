1b8c6abb2825669a60c1850c737672c82beabe4d
Dreammjow <50238852+dreammjow@users.noreply.github.com>
2023-02-13 12:18:06 +0000

    changes

--- src/pages/downloads/verification.js
+++ src/pages/downloads/verification.js
@@ -6,132 +6,149 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "../index.module.css";
 
 function Header() {
-	return (
-	<header className={clsx("hero hero--primary", styles.downloads, styles.heroBanner)}>
-		<div className="container">
-			<h1 className="hero__title">Verification</h1>
-		</div>
-	</header>
-	)
+  return (
+    <header
+      className={clsx(
+        "hero hero--primary",
+        styles.downloads,
+        styles.heroBanner
+      )}
+    >
+      <div className="container">
+        <h1 className="hero__title">Verification</h1>
+      </div>
+    </header>
+  );
 }
 
 function Info() {
-	return (
-		<section style={{ paddingTop: `4rem` }}>
-			<div className="container">
-				<div className="row">
-					<div className={clsx("col col--12")}>
-						<div className="text--left padding-horiz--md">
-							<p>
-							ReviOS ISOs not provided through this site are considered non-verifiable and should NOT be deployed.
-							</p>
-							<p>
-							Please confirm that you have a legitimate copy of ReviOS by hash-checking the ISO before deploying it on your system.
-							</p>
-							<p>
-							Ensure the site hasn't been modified in any way, e.g. with inspect element and the likes 
-							- this can be done by accessing the site through different browsers on multiple devices and securing that they match.
-							</p>
-							<p>
-							Below you'll find a hash list of old and new ReviOS builds which should be used as a validated reference in the verification process.
-							</p>
-							<p>
-							Revision only support the builds found on the download page of this site.
-							</p>
-							<p>Unsupported builds are superseded by newer ones and are no longer part of the ReviOS support channel found on our Discord server.</p>
-							<p>
-							<b>Supported builds are checkmarked (✅)</b>
-							</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	)
+  return (
+    <section style={{ paddingTop: `4rem` }}>
+      <div className="container">
+        <div className="row">
+          <div className={clsx("col col--12")}>
+            <div className="text--left padding-horiz--md">
+              <p>
+                ReviOS ISOs not provided through this site are considered
+                non-verifiable and should NOT be deployed.
+              </p>
+              <p>
+                Please confirm that you have a legitimate copy of ReviOS by
+                hash-checking the ISO before deploying it on your system.
+              </p>
+              <p>
+                Ensure the site hasn't been modified in any way, e.g. with
+                inspect element and the likes - this can be done by accessing
+                the site through different browsers on multiple devices and
+                securing that they match.
+              </p>
+              <p>
+                Below you'll find a hash list of old and new ReviOS builds which
+                should be used as a validated reference in the verification
+                process.
+              </p>
+              <p>
+                Revision only support the builds found on the download page of
+                this site.
+              </p>
+              <p>
+                Unsupported builds are superseded by newer ones and are no
+                longer part of the ReviOS support channel found on our Discord
+                server.
+              </p>
+              <p>
+                <b>Supported builds are checkmarked (✅)</b>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 function Hashes() {
-	const url = "https://api.stasium.dev/v1/revios/hashes"
-	const [isLoading, setLoading] = useState(true);
-	const [hashes, setHashes] = useState();
+  const url = "https://api.stasium.dev/v1/revios/hashes";
+  const [isLoading, setLoading] = useState(true);
+  const [hashes, setHashes] = useState();
 
-	useEffect(async () => {
-		let api = await fetch(url, {
-			method: 'GET'
-		});
+  useEffect(async () => {
+    let api = await fetch(url, {
+      method: "GET",
+    });
 
-		let result = await api.json();
-		
-		console.log(result)
-		setHashes(result.reverse());
-		setLoading(false);	
-	}, [])
+    let result = await api.json();
 
-	if (isLoading) {
-		return (
-			<section style={{ paddingTop: `4rem` }}>
-			<div className="container">
-				<div className="row lds-center">
-					<div className="lds-ring">
-						<div></div>
-						<div></div>
-						<div></div>
-						<div></div>
-					</div>
-				</div>
-			</div>
-		</section>
-		)
-	}
+    console.log(result);
+    setHashes(result.reverse());
+    setLoading(false);
+  }, []);
 
-	return (
-		<div>
-			{hashes.map((props, idx) => (
-				<HashesSection key={idx} {...props} />
-			))}
-		</div>
-	)
+  if (isLoading) {
+    return (
+      <section style={{ paddingTop: `4rem` }}>
+        <div className="container">
+          <div className="row lds-center">
+            <div className="lds-ring">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+
+  return (
+    <div>
+      {hashes.map((props, idx) => (
+        <HashesSection key={idx} index={idx} {...props} />
+      ))}
+    </div>
+  );
 }
 
-function HashesSection({ name, md5, sha256 }) {
-	return (
-		<section>
-			<div className="container">
-				<div className="row">
-					<div className={clsx("col col-12")}>
-						<div className="text--left padding-horiz--md">
-							<hr></hr>
-							<h3>{ name.includes("22.12") ? "✅" + name : name }</h3>
-							<p>MD5:</p>
-							<pre>
-								<code>{md5}</code>
-							</pre>
-							<p>SHA-256:</p>
-							<pre>
-								<code>{sha256}</code>
-							</pre>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	)
+function HashesSection({ index, name, md5, sha256 }) {
+  return (
+    <section>
+      <div className="container">
+        <div className="row">
+          <div className={clsx("col col-12")}>
+            <div className="text--left padding-horiz--md">
+              <hr />
+              <h3>{index == 0 || index == 1 ? "✅" + name : name}</h3>
+              <p>MD5:</p>
+              <pre>
+                <code>{md5}</code>
+              </pre>
+              <p>SHA-256:</p>
+              <pre>
+                <code>{sha256}</code>
+              </pre>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default function Main() {
-	const { siteConfig } = useDocusaurusContext();
-	return (
-		<Layout
-			title={`${siteConfig.title}`}
-			description="ReviOS aspires to re-create what Windows as an operating system should have been - easy and simple.
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description="ReviOS aspires to re-create what Windows as an operating system should have been - easy and simple.
 		With the main audience being gamers, power-users and enthusiasts, we understand that performance, speed and low latency is obligatory, which is why great effort has been invested into making ReviOS a capable, efficient yet private operating system.
 		Being naturally light on resources, footprint and size it also comes in as a great fit for low-end systems. "
-		>
-			<Header />
-			<main>
-				<Info />
-				<Hashes />
-			</main>
-		</Layout>
-	);
+    >
+      <Header />
+      <main>
+        <Info />
+        <Hashes />
+      </main>
+    </Layout>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,72 +8,97 @@ import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import styles from "./index.module.css";
 
 function HomepageHeader() {
-	const { siteConfig } = useDocusaurusContext();
-	return (
-		<header className={clsx("hero hero--primary", styles.heroBanner)}>
-			<div className="container">
-				<h1 className="hero__title">{siteConfig.title}</h1>
-				<p className="hero__subtitle">{siteConfig.tagline}</p>
-				<div className={styles.buttons}>
-					<Link
-						className="button button--secondary button--lg"
-						to="/downloads"
-					>
-						Download Revision
-					</Link>
-				</div>
-			</div>
-		</header>
-	);
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--outline button--lg"
+            to="/downloads"
+          >
+            Download ReviOS
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 function ReviAbout() {
-	return (
-		<section style={{padding: `4rem 0`}}>
-			<div className="container">
-				<div className="row">
-					<div className={clsx("col col--8")}>
-						<div className="text--left">
-						<p>ReviOS aspires to re-create what Windows as an operating system should have been -
-							easy and simple.</p>
-						<p>With the main audience being gamers, power-users and enthusiasts, we understand
-							that performance, speed and low latency is obligatory, which is why great effort has been invested into making
-							ReviOS a capable, efficient yet private operating system.</p>
-						<p>Being naturally light on resources, footprint and size it also comes in as a great fit for low-end systems. </p>
-						</div>
-					</div>
-					<div className={clsx("col col--3")}>
-						<div className={styles.buttonPair}>
-							<Link to="/faq">
-								<button className={styles.homeButton + " button button--outline button--secondary button--lg"} type="button">🤔 FAQ</button>
-							</Link>
-							<Link to="/changelog">
-								<button className={styles.homeButton + " button button--outline button--secondary button--lg"} type="button">📄 Changelog</button>
-							</Link>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section style={{ padding: `4rem 0` }}>
+      <div className="container">
+        <div className="row">
+          <div className={clsx("col col--8")}>
+            <div className="text--left">
+              <p>
+                ReviOS aspires to re-create what Windows as an operating system
+                should have been - easy and simple.
+              </p>
+              <p>
+                With the main audience being gamers, power-users and
+                enthusiasts, we understand that performance, speed and low
+                latency is obligatory, which is why great effort has been
+                invested into making ReviOS a capable, efficient yet private
+                operating system.
+              </p>
+              <p>
+                Being naturally light on resources, footprint and size it also
+                comes in as a great fit for low-end systems.
+              </p>
+            </div>
+          </div>
+          <div className={clsx("col")}>
+            <div className={styles.buttonPair}>
+              <Link to="/faq">
+                <button
+                  className={
+                    styles.homeButton +
+                    " button button--outline button--secondary button--lg"
+                  }
+                  type="button"
+                >
+                  🤔 FAQ
+                </button>
+              </Link>
+              <Link to="/changelog">
+                <button
+                  className={
+                    styles.homeButton +
+                    " button button--outline button--secondary button--lg"
+                  }
+                  type="button"
+                >
+                  📄 Changelog
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default function Home() {
-	const { siteConfig } = useDocusaurusContext();
-	return (
-		<Layout
-			title={`${siteConfig.title}`}
-			description="ReviOS aspires to re-create what Windows as an operating system should have been - easy and simple.
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description="ReviOS aspires to re-create what Windows as an operating system should have been - easy and simple.
 
 	  With the main audience being gamers, power-users and enthusiasts, we understand that performance, speed and low latency is obligatory, which is why great effort has been invested into making ReviOS a capable, efficient yet private operating system.
 	  
 	  Being naturally light on resources, footprint and size it also comes in as a great fit for low-end systems. "
-		>
-			<HomepageHeader />
-			<main>
-				<ReviAbout />
-				<HomepageFeatures />
-			</main>
-		</Layout>
-	);
+    >
+      <HomepageHeader />
+      <main>
+        <ReviAbout />
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
 }
