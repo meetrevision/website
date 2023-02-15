deddcd0e9645a88e7740c1254e2eca45abb86f1b
RainerZufahl <crafter45@gmail.com>
2023-02-15 18:26:36 +0100

    first restyle of homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,14 +14,11 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--outline button--lg"
-            to="/downloads"
-          >
+        {/* <div className={styles.buttons}>
+          <Link className="button button--secondary button--outline button--lg" to="/downloads">
             Download ReviOS
           </Link>
-        </div>
+        </div> */}
       </div>
     </header>
   );
@@ -29,33 +26,25 @@ function HomepageHeader() {
 
 function RevisionAbout() {
   return (
-    <section className="padding-top--lg">
-      <div className="container">
+    <section className="margin-vert--lg">
+      <div className="container padding-horiz--xl">
         <h1 className="text--center">Who we are</h1>
         <div className="text--left">
           <p>
-            A global community dedicated to free information and collaboration.
-            We value cultural diversity and embrace open source principles.
-            Every mind is unique and every contribution is valued.
+            A global community dedicated to free information and collaboration. We value cultural diversity and embrace
+            open source principles. Every mind is unique and every contribution is valued.
           </p>
         </div>
-        <div className="text--center">
-          <h2>Find us on:</h2>
-          <div className="row" style={{ width: "30em", margin: "auto" }}>
-            {/* These Icons need links to the respective pages */}
-            <Link to="https://discord.gg/962y4pU" className="col">
-              <p className="fa-brands fa-discord" style={{ fontSize: "3em" }} />
-              <p>Discord</p>
-            </Link>
-            <Link to="https://discord.gg/962y4pU" className="col">
-              <p className="fa-brands fa-github" style={{ fontSize: "3em" }} />
-              <p>GitHub</p>
-            </Link>
-            <Link to="https://discord.gg/962y4pU" className="col">
-              <p className="fa-brands fa-reddit" style={{ fontSize: "3em" }} />
-              <p>Reddit</p>
-            </Link>
-          </div>
+
+        <div className="row margin-horiz--xl">
+          <Link to="https://discord.gg/962y4pU" className="col col--4 button button--secondary button--outline">
+            <i className="fa-brands fa-discord" />
+            <span className="padding-left--sm">Discord</span>
+          </Link>
+          <Link to="https://github.com/MeetRevision" className="col col--4 button button--secondary button--outline">
+            <i className="fa-brands fa-github" />
+            <span className="padding-left--sm">GitHub</span>
+          </Link>
         </div>
       </div>
     </section>
@@ -65,55 +54,36 @@ function RevisionAbout() {
 function ReviAbout() {
   return (
     <section>
-      <div className="container padding-top--lg">
+      <div className={clsx("container padding-top--lg padding-bottom--xl mockup-image--1")}>
         <h1 className="text--center">Our main project: ReviOS</h1>
         <p>
-          <b>ReviOS</b>, a customized version of Windows 10 with improved
-          performance, stability, and compatibility. It aspires to re-create
-          what Windows as an operating system should have been - easy and
-          simple.
+          <b>ReviOS</b>, a customized version of Windows 10 with improved performance, stability, and compatibility. It
+          aspires to re-create what Windows as an operating system should have been - easy and simple.
         </p>
         <p>
-          With the main audience being gamers, power-users and enthusiasts, we
-          understand that performance, speed and low latency is obligatory,
-          which is why great effort has been invested into making ReviOS a
-          capable, <b>efficient yet private</b> operating system.
+          With the main audience being gamers, power-users and enthusiasts, we understand that performance, speed and
+          low latency is obligatory, which is why great effort has been invested into making ReviOS a capable,{" "}
+          <b>efficient yet private</b> operating system.
         </p>
         <p>
-          Being naturally light on resources, footprint and size it also comes
-          in as a great fit for low-end systems.
+          Being naturally light on resources, footprint and size it also comes in as a great fit for low-end systems.
         </p>
-      </div>
-      <div className="row" style={{ width: "70vw", margin: "auto" }}>
-        <div className={styles.buttons + " col"}>
-          <Link to="/downloads">
-            <button
-              className="button button--secondary button--lg"
-              type="button"
-            >
-              Download Revision
-            </button>
-          </Link>
-        </div>
-        <div className={styles.buttons + " col"}>
-          <Link to="/faq">
-            <button
-              className="button button--outline button--secondary button--lg"
-              type="button"
-            >
-              Frequent Questions
-            </button>
-          </Link>
-        </div>
-        <div className={styles.buttons + " col"}>
-          <Link to="/changelog">
-            <button
-              className="button button--outline button--secondary button--lg"
-              type="button"
-            >
-              Changelog
-            </button>
-          </Link>
+        <div className="row">
+          <div className="col--4">
+            <Link to="/downloads">
+              <a className="button button--secondary button--lg">Download Revision</a>
+            </Link>
+          </div>
+          <div className="col--4">
+            <Link to="/faq">
+              <a className="button button--outline button--secondary button--lg">Frequent Questions</a>
+            </Link>
+          </div>
+          <div className="col--4">
+            <Link to="/changelog">
+              <a className="button button--outline button--secondary button--lg">Changelog</a>
+            </Link>
+          </div>
         </div>
       </div>
     </section>
