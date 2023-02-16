ce48bd1273c7cb12f3589ff1563e4900e44d5dd2
RainerZufahl <crafter45@gmail.com>
2023-02-17 00:20:22 +0100

    minor design changes

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import clsx from "clsx";
 import styles from "./styles.module.css";
 
 const FeatureList = [
@@ -11,24 +10,19 @@ const FeatureList = [
     description: (
       <>
         <p>
-          Windows is notoriously known for its less-than-ideal performance,
-          especially when compared to its predecessors.
+          Windows is notoriously known for its less-than-ideal performance, especially when compared to its
+          predecessors.
         </p>
 
         <p>
-          ReviOS attempts to speed up the system, by eliminating redundant tasks
-          and services running in the background, without sacrificing on
-          stability.
+          ReviOS attempts to speed up the system, by eliminating redundant tasks and services running in the background,
+          without sacrificing on stability.
         </p>
 
-        <p>
-          Better yet, framerate stability is greatly improved, meaning your FPS
-          is less prone to fluctuation.
-        </p>
+        <p>Better yet, framerate stability is greatly improved, meaning your FPS is less prone to fluctuation.</p>
 
         <p>
-          Benchmarks comparing stock Windows 10 and ReviOS can be found{" "}
-          <a href="https://imgur.com/a/v72g8vA">here</a>.
+          Benchmarks comparing stock Windows 10 and ReviOS can be found <a href="https://imgur.com/a/v72g8vA">here</a>.
         </p>
       </>
     ),
@@ -41,19 +35,18 @@ const FeatureList = [
     description: (
       <>
         <p>
-          Microsoft's Windows operating system is infamous for its
-          privacy-intrusive behaviour towards consumers, which is why ReviOS
-          takes the privacy aspect into serious account.
+          Microsoft's Windows operating system is infamous for its privacy-intrusive behaviour towards consumers, which
+          is why ReviOS takes the privacy aspect into serious account.
         </p>
 
         <p>
-          A plethora of telemetry backdoors have been terminated including, but
-          is not limited to: UWP/AppX Apps, Windows Update and Cortana.
+          A plethora of telemetry backdoors have been terminated including, but is not limited to: UWP/AppX Apps,
+          Windows Update and Cortana.
         </p>
 
         <p>
-          Data collection and reporting increase the system load, and as a
-          bonus, that too is severely reduced, hereby promoting performance.
+          Data collection and reporting increase the system load, and as a bonus, that too is severely reduced, hereby
+          promoting performance.
         </p>
       </>
     ),
@@ -65,19 +58,14 @@ const FeatureList = [
     imgSubTitle: "Stable, compatible and reliable",
     description: (
       <>
+        <p>Based on a multi-layered approach, securing retained and comprehensive stability.</p>
         <p>
-          Based on a multi-layered approach, securing retained and comprehensive
-          stability.
+          Reinforcing performance and privacy by stripping out bloatware that halts the system, and cutting down on the
+          amount of components - resulting in reduced OS size.
         </p>
         <p>
-          Reinforcing performance and privacy by stripping out bloatware that
-          halts the system, and cutting down on the amount of components -
-          resulting in reduced OS size.
-        </p>
-        <p>
-          ReviOS attempts to enhance what can be enhanced, leaving the
-          subjective things to the user. A reliable, yet flexible baseline to
-          build upon.
+          ReviOS attempts to enhance what can be enhanced, leaving the subjective things to the user. A reliable, yet
+          flexible baseline to build upon.
         </p>
         <p>Add, modify and extend to your heart's content.</p>
       </>
@@ -91,14 +79,12 @@ const FeatureList = [
     description: (
       <>
         <p>
-          Revision isn't only about ReviOS, it's about the thriving community
-          behind it. A community that help push the boundaries of optimization
-          and aid the vision of a more inspiring digital experience.
+          Revision isn't only about ReviOS, it's about the thriving community behind it. A community that help push the
+          boundaries of optimization and aid the vision of a more inspiring digital experience.
         </p>
 
         <p>
-          <a href="https://discord.gg/962y4pU">Join our Discord</a> for
-          exclusive ReviOS support and much more.
+          <a href="https://discord.gg/962y4pU">Join our Discord</a> for exclusive ReviOS support and much more.
         </p>
       </>
     ),
@@ -108,20 +94,18 @@ const FeatureList = [
 function Feature({ img, imgTitle, title, imgSubTitle, description }) {
   return (
     <section className="margin-vert--lg">
-      <div className="container">
-        <div className="row">
-          <div className={clsx("col col--3")}>
-            <div className="text--center" style={{ alignItems: "center" }}>
-              <img className={styles.featureImg} src={img} />
-              <h2 className={styles.imgTitle}>{imgTitle}</h2>
-              <h4 className={styles.imgSubTitle}>{imgSubTitle}</h4>
-            </div>
+      <div className="container row" style={{ alignItems: "center" }}>
+        <div className="col col--3">
+          <div className="text--center">
+            <img className={styles.featureImg} src={img} />
+            <h2 className={styles.imgTitle}>{imgTitle}</h2>
+            <h4 className={styles.imgSubTitle}>{imgSubTitle}</h4>
           </div>
-          <div className={clsx("col col--9")}>
-            <div className="text--left">
-              <h3 className={styles.title}>{title}</h3>
-              <p className={styles.description}>{description}</p>
-            </div>
+        </div>
+        <div className="col col--9">
+          <div className="text--left">
+            <h3 className={styles.title}>{title}</h3>
+            <span className={styles.description}>{description}</span>
           </div>
         </div>
       </div>
